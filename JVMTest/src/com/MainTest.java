ed58292c520acacc9f640a30cff3f9b9b4301d6f
yangsumeng <584903541@qq.com>
2019-06-13 23:26:50 +0800

    dev提交

--- JVMTest/src/com/MainTest.java
+++ JVMTest/src/com/MainTest.java
@@ -6,7 +6,7 @@ public class MainTest
     public static void main(String[] args)
     {
        while(true){
-           System.out.println(123);
+           System.out.println(1231111);
        }
         
     }
